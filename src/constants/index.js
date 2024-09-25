0211fa3d9ee1a6eff883cb23a7ba9e70165093b5
mrTSB <tsbhathal@gmail.com>
2024-09-24 17:40:30 -0700

    BAtch Update

---
+++ src/constants/index.js
@@ -0,0 +1,268 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    brain,
+    sail,
+    rdp,
+    zibdy,
+    ucsd,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Software Engineer",
+      icon: mobile,
+    },
+    {
+      title: "NLP Researcher",
+      icon: backend,
+    },
+    {
+      title: "Full-Stack Developer",
+      icon: web,
+    },
+    {
+      title: "Product Manager",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "NLP Student Researcher",
+      company_name: "Stanford AI Laboratory",
+      icon: sail,
+      iconBg: "#E6DEDD",
+      date: "May 2024 - Present",
+      points: [
+        "Co-Advised by Azalia Mirhoseini and Christopher Ré. Working on RAG."
+      ],
+    },
+    {
+      title: "Product Management Intern",
+      company_name: "Brain Corp",
+      icon: brain,
+      iconBg: "#E6DEDD",
+      date: "June 2024 - Sept 2024",
+      points: [
+        "AI Solutions for Autonomous Robot functionality and path generation."
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "RapDevPro",
+      icon: rdp,
+      iconBg: "#FFFFFF",
+      date: "Dec 2022 - Oct 2023",
+      points: [
+        "Developed the entire admin panel of social media & gaming application. Responsible for some features on the mobile application as well."
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "ZibdyHealth",
+      icon: zibdy,
+      iconBg: "#E6DEDD",
+      date: "June 2023 - Sept 2023",
+      points: [
+        "Developed next generation iOS and web application."
+      ],
+    },
+    {
+      title: "Computational Biology Research Assistant",
+      company_name: "UC San Diego, Karsten Zengler Lab",
+      icon: ucsd,
+      iconBg: "#E6DEDD",
+      date: "June 2022 - Sept 2023",
+      points: [
+        "Working on simulation models at the microbial level."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
