f161cb506f617d8dc8aff75b1e88538c89cd8825
mrTSB <tsbhathal@gmail.com>
2025-02-22 11:33:00 -0800

    Small change

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -84,7 +84,9 @@ const Works = () => {
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          and manage projects effectively. Most of my work is closed source as it
+          was completed at internships, jobs, or classes that prohibit me from posting
+          code on github.
         </motion.p>
       </div>
 
